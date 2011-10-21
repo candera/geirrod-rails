d5c194b46eeb27671eff4d6e4831272b140aa8e8
Craig Andera <candera@wangdera.com>
2011-10-21 16:09:07 -0400

    Add initial scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeirrodRails::Application.config.secret_token = 'dfe442eee954dba32bd7d5425b89cc2e55c80f64359ef564b985832b8aaf57ee6176295211b630de3a72b9c6031e99f5ee0bf251a1b214f7df0ff557b372356a'
